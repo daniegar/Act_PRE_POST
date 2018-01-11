3c001d1f4765eadb1c5c3208ef83f71c8e54923e
daniegar <danielgarcia@cencosud.cl>
2018-01-11 16:26:36 -0300

    Update estado_clientes.sh

--- Act_PRE_POST/estado_clientes.sh
+++ Act_PRE_POST/estado_clientes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ISCL=$(cat [ruta alta cliente] | grep -E "QmName_1 = |CLineName_1 = " |grep -B1 "ISCL" |grep QmName |awk '{print $3}')
-FTP=$(cat [ruta alta cliente] | grep -E "QmName_1 = |CLineName_1 = " |grep -B1 "FTP" |grep QmName |awk '{print $3}')
+internos=$(cat [ruta alta cliente] | grep -E "QmName_1 = |CLineName_1 = " |grep -B1 "[protocolo clientes internos]" |grep QmName |awk '{print $3}')
+externos=$(cat [ruta alta cliente] | grep -E "QmName_1 = |CLineName_1 = " |grep -B1 "[protocolo clientes externos]" |grep QmName |awk '{print $3}')
 
-for i in $ISCL; do
+for i in $internos; do
         [listar colas] $i >/dev/null
         if [ $? -ne 0 ]; then
                 echo "Servidor $i No está en uso"
